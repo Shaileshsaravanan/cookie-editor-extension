e165145c87f64bc0542f2fcf84efc28552f2ebad
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-08-15 16:04:23 +0530

    background.js update + more index.js upgrades

--- background.js
+++ background.js
@@ -1,41 +1,114 @@
-'use strict';
+"use strict";
 
-var once = () => {
-  // FAQs & Feedback
-  chrome.storage.local.get({
-    'version': null,
-    'faqs': navigator.userAgent.indexOf('Firefox') === -1,
-    'last-update': 0
-  }, prefs => {
-    const version = chrome.runtime.getManifest().version;
-
-    if (prefs.version ? (prefs.faqs && prefs.version !== version) : true) {
-      const now = Date.now();
-      const doUpdate = (now - prefs['last-update']) / 1000 / 60 / 60 / 24 > 30;
-      chrome.storage.local.set({
-        version,
-        'last-update': doUpdate ? Date.now() : prefs['last-update']
-      }, () => {
-        // do not display the FAQs page if last-update occurred less than 30 days ago.
-        if (doUpdate) {
-          const p = Boolean(prefs.version);
-          chrome.tabs.create({
-            url: chrome.runtime.getManifest().homepage_url + '?version=' + version +
-              '&type=' + (p ? ('upgrade&p=' + prefs.version) : 'install'),
-            active: p === false
-          });
-        }
-      });
-    }
+const notify = (message) =>
+  chrome.notifications.create({
+    type: "basic",
+    title: chrome.runtime.getManifest().name,
+    message,
   });
 
-  {
-    const {name, version} = chrome.runtime.getManifest();
-    chrome.runtime.setUninstallURL(
-      chrome.runtime.getManifest().homepage_url + '?rd=feedback&name=' + name + '&version=' + version
+// context me
+{
+  const once = () => {
+    chrome.contextMenus.create({
+      id: "search",
+      title: "Search Cookies by Domain",
+      contexts: ["browser_action"],
+    });
+    chrome.contextMenus.create({
+      id: "open",
+      title: "Open Cookie Editor in new Tab",
+      contexts: ["browser_action"],
+    });
+  };
+  chrome.runtime.onStartup.addListener(once);
+  chrome.runtime.onInstalled.addListener(once);
+}
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "search") {
+    let search;
+    const next = () => {
+      if (search) {
+        chrome.tabs.create({
+          url: "/data/popup/index.html?search=" + encodeURIComponent(search),
+        });
+      }
+    };
+    if (/Firefox/.test(navigator.userAgent)) {
+      chrome.tabs.executeScript(
+        {
+          code: `window.prompt('Please enter the domain (e.g.: www.google.com)')`,
+          runAt: "document_start",
+        },
+        (a) => {
+          if (chrome.runtime.lastError) {
+            notify(chrome.runtime.lastError.message);
+          } else if (a) {
+            search = a[0];
+            next();
+          }
+        },
+      );
+    } else {
+      search = window.prompt("Please enter the domain (e.g.: www.google.com)");
+      next();
+    }
+  } else if (info.menuItemId === "open") {
+    chrome.tabs.create({
+      url: "/data/popup/index.html?tabId=" + encodeURIComponent(tab.id),
+    });
+  }
+});
+
+/* FAQs & Feedback */
+{
+  const {
+    management,
+    runtime: { onInstalled, setUninstallURL, getManifest },
+    storage,
+    tabs,
+  } = chrome;
+  if (navigator.webdriver !== true) {
+    const page = getManifest().homepage_url;
+    const { name, version } = getManifest();
+    onInstalled.addListener(({ reason, previousVersion }) => {
+      management.getSelf(
+        ({ installType }) =>
+          installType === "normal" &&
+          storage.local.get(
+            {
+              faqs: true,
+              "last-update": 0,
+            },
+            (prefs) => {
+              if (reason === "install" || (prefs.faqs && reason === "update")) {
+                const doUpdate =
+                  (Date.now() - prefs["last-update"]) / 1000 / 60 / 60 / 24 >
+                  45;
+                if (doUpdate && previousVersion !== version) {
+                  tabs.create({
+                    url:
+                      page +
+                      "?version=" +
+                      version +
+                      (previousVersion ? "&p=" + previousVersion : "") +
+                      "&type=" +
+                      reason,
+                    active: reason === "install",
+                  });
+                  storage.local.set({ "last-update": Date.now() });
+                }
+              }
+            },
+          ),
+      );
+    });
+    setUninstallURL(
+      page +
+        "?rd=feedback&name=" +
+        encodeURIComponent(name) +
+        "&version=" +
+        version,
     );
   }
-};
-chrome.runtime.onStartup.addListener(once);
-chrome.runtime.onInstalled.addListener(once);
-
+}
--- data/popup/index.js
+++ data/popup/index.js
@@ -2,64 +2,79 @@
 'use strict';
 
 var args = new URLSearchParams(location.search);
+var search = args.get('search') || null;
 var tabId = args.get('tabId') || null;
 if (tabId) {
   tabId = Number(tabId);
-  document.body.dataset.popup = false;
 }
+document.body.dataset.popup = !tabId && !search;
 
-const init = () => chrome.tabs.executeScript(tabId, {
-  allFrames: true,
-  code: 'Object.assign({top: window.top === window}, document.location);',
-  runAt: 'document_start'
-}, a => {
-  //
+const init = a => {
+  const hostnames = a.map(o => o.origin).filter((h, i, l) => l.indexOf(h) === i);
+  const top = a.filter(o => o.top).shift();
+  hostnames.forEach(h => {
+    const tbody = table.section(h, h === top.origin);
+    cookies.all(h).then(cs => {
+      tbody.add(cs, h);
+      tbody.count(cs.length);
+      if (h === top.origin && cs.length) {
+        tbody.first().active();
+      }
+      // when there is no cookie
+      if (cs.length === 0 && h === top.origin) {
+        editor.origin = top.origin;
+        document.getElementById('domain').value = top.hostname;
+      }
+    });
+  });
+  // resizing for expanded mode
+  if (document.body.dataset.popup === 'false') {
+    const resizable = new Resizable(document.getElementById('header'), {
+      offset: -3,
+      width: 5,
+      min: 5,
+      persist: true
+    });
+    resizable.on('draw', () => {
+      table.emit('resizing', resizable.array());
+    });
+    resizable.init();
+  }
+  // remove loader
   document.getElementById('loading').remove();
-  //
-  if (chrome.runtime.lastError) {
-    document.getElementById('msg').textContent = chrome.runtime.lastError.message;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (search) {
+    if (search.indexOf('://') === -1) {
+      search = 'http://' + search;
+    }
+    init([Object.assign(new URL(search), {
+      top: true
+    })]);
   }
   else {
-    const hostnames = a.map(o => o.origin).filter((h, i, l) => l.indexOf(h) === i);
-    const top = a.filter(o => o.top).shift();
-    hostnames.forEach(h => {
-      const tbody = table.section(h, h === top.origin);
-      cookies.all(h).then(cs => {
-        tbody.add(cs, h);
-        tbody.count(cs.length);
-        if (h === top.origin && cs.length) {
-          tbody.first().active();
-        }
-        // when there is no cookie
-        if (cs.length === 0 && h === top.origin) {
-          editor.origin = top.origin;
-          document.getElementById('domain').value = top.hostname;
-        }
-      });
+    chrome.tabs.executeScript(tabId, {
+      allFrames: true,
+      code: 'Object.assign({top: window.top === window}, document.location);',
+      runAt: 'document_start'
+    }, a => {
+      if (chrome.runtime.lastError) {
+        document.getElementById('loading').remove();
+        document.getElementById('msg').textContent = chrome.runtime.lastError.message;
+      }
+      else {
+        init(a);
+      }
     });
-    // resizing for expanded mode
-    if (tabId) {
-      const resizable = new Resizable(document.getElementById('header'), {
-        offset: -3,
-        width: 5,
-        min: 5,
-        persist: true
-      });
-      resizable.on('draw', () => {
-        table.emit('resizing', resizable.array());
-      });
-      resizable.init();
-    }
   }
 });
-
-document.addEventListener('DOMContentLoaded', () => window.setTimeout(init, 500));
 
 table.on('select', tr => {
   editor.attr({
     disabled: false
   }, '[data-cmd=remove]');
-  editor.update(tr.cookie, tr.origin);
+  editor.update(tr.cookie, tr.origin, tr);
 });
 table.on('no-select', () => {
   editor.attr({
@@ -74,19 +89,73 @@ table.on('multi-select', cookies => {
 });
 
 editor.create(e => {
-  const tbody = e.target.closest('details').querySelector('tbody');
-  const origin = e.target.dataset.origin;
-  const tr = tbody.add([{
-    name: '',
-    value: '',
-    expirationDate: Date.now() / 1000 + 24 * 60 * 60,
-    session: false
-  }], origin).shift();
-  const input = tr.querySelector('input[type=radio]');
-  input.checked = true;
-  input.dispatchEvent(new Event('change', {bubbles: true}));
-  input.scrollIntoView();
-  editor.focus();
+  const next = list => {
+    const tbody = e.target.closest('details').querySelector('tbody');
+    const origin = e.target.dataset.origin;
+    const trs = tbody.add(list, origin);
+    console.log(list, origin);
+    for (const tr of trs) {
+      tr.dataset.edited = true;
+    }
+    const input = trs[0].querySelector('input[type=radio]');
+    input.checked = true;
+    input.dispatchEvent(new Event('change', {bubbles: true}));
+    input.scrollIntoView();
+    editor.focus();
+  };
+
+  if (e.shiftKey) {
+    const input = document.createElement('input');
+    input.style.display = 'none';
+    input.type = 'file';
+    input.accept = '.json, .txt';
+    input.acceptCharset = 'utf-8';
+
+    document.body.appendChild(input);
+    input.onchange = () => {
+      if (input.value !== input.initialValue) {
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          input.remove();
+          if (file.name.endsWith('.txt')) {
+            const list = [];
+            const entries = reader.result.split('\n').filter(a => a && a[0] !== '#');
+            for (const entry of entries) {
+              const [domain, sub, path, secure, expirationDate, name, value] = entry.split('\t');
+              list.push({
+                path,
+                secure: secure === 'TRUE',
+                expirationDate: parseFloat(expirationDate),
+                name,
+                value,
+                session: false
+              });
+            }
+            next(list);
+          }
+          else if (file.name.endsWith('.json')) {
+            const json = JSON.parse(reader.result).map(a => {
+              delete a.domain;
+              a.session = false;
+              return a;
+            });
+            next(json);
+          }
+        };
+        reader.readAsText(file, 'utf-8');
+      }
+    };
+    input.click();
+  }
+  else {
+    next([{
+      name: '',
+      value: '',
+      expirationDate: Date.now() / 1000 + 24 * 60 * 60,
+      session: false
+    }]);
+  }
 });
 
 editor.reset(() => table.query('input').active());
@@ -96,7 +165,7 @@ editor.save(() => {
 
   cookies.replace(origin, cookie, nCookie).then(cookie => {
     const tr = table.write(table.query(), cookie, origin);
-    editor.update(cookie, origin);
+    editor.update(cookie, origin, tr);
     // move to the bottom
     table.append(tr);
   }).catch(e => {
@@ -113,7 +182,16 @@ editor.save(() => {
 });
 
 editor.remove(() => {
-  table.selected().map(({cookie, origin, tr}) => {
+  const items = table.selected();
+  if (items.length === 0) {
+    const tr = document.querySelector('#cookies input[type=radio]:checked').closest('tr');
+    items.push({
+      tr,
+      cookie: tr.cookie,
+      origin: tr.origin
+    });
+  }
+  items.map(({cookie, origin, tr}) => {
     cookies.remove(origin, cookie).then(table.remove(tr));
   });
 });
@@ -126,15 +204,28 @@ editor.expand(() => chrome.tabs.query({
     url: '/data/popup/index.html?' + args.toString()
   }, () => window.close());
 }));
-editor.export(() => {
+editor.export(e => {
   const cookies = document.querySelector('[data-cmd=export]').cookies;
-  const text = JSON.stringify(cookies, null, '  ');
-  const blob = new Blob([text], {type: 'application/json'});
+  let text = JSON.stringify(cookies, null, '  ');
+  if (e.shiftKey) {
+    text = cookies.map(cookie => {
+      return [
+        cookie.domain,
+        'TRUE',
+        cookie.path,
+        cookie.secure.toString().toUpperCase(),
+        cookie.expirationDate,
+        cookie.name,
+        cookie.value].join('\t');
+    }).join('\n');
+  }
+  const blob = new Blob([text], {
+    type: e.shiftKey ? 'text/plain' : 'application/json'
+  });
   const objectURL = URL.createObjectURL(blob);
   Object.assign(document.createElement('a'), {
     href: objectURL,
-    type: 'application/json',
-    download: 'exported-cookies.json'
+    download: 'exported-cookies.' + (e.shiftKey ? 'txt' : 'json')
   }).dispatchEvent(new MouseEvent('click'));
   setTimeout(() => URL.revokeObjectURL(objectURL));
 });
